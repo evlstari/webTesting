04e90fd3dea7775bf2c59e40eedeb5957aa6f17a
pii <evlstari99@gmail.com>
2025-07-09 15:22:50 +0700

    hashing password2

--- api/login.js
+++ api/login.js
@@ -11,6 +11,22 @@ bcrypt.hash(password, saltRounds, (err, hash) => {
     console.log('Generated hash for "password123":', hash);
 });
 
+
+// Data pengguna dummy. Dalam aplikasi nyata, ini dari database.
+
+const users = [
+
+    {
+
+        email: 'user@example.com',
+
+        // Password: "password123". Hash ini dibuat dengan bcrypt.hash('password123', 10);
+
+        passwordHash: '$2b$10$WzUlb9m7utHar.bnwCPGReWa9wFtKsGqqzTPsyz2i7.KUgbKox4L.'
+
+    }
+
+];
 // Fungsi utama untuk Serverless Function
 module.exports = async (req, res) => {
     // Pastikan request adalah POST
